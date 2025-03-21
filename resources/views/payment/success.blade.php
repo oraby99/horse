68fac4d5ac9f913cea5bebb614a123c2c6917f57
unknown <mohamedmagdymohamed982@gmail.com>
2025-03-21 20:47:48 +0200

    Handle Payment Error

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -3,7 +3,7 @@
 <head><title>Payment Success</title></head>
 <body>
     <h1>Payment Successful!</h1>
-    <p>Order ID: {{ $data['orderReferenceNumber'] ?? 'N/A' }}</p>
+    <p>Order ID: {{ $data['paymentId'] ?? 'N/A' }}</p>
     <a href="{{ route('cart') }}">Back to Cart</a>
 </body>
 </html>
